5099a5fcdaa8c45fcfafb7fa1cc4ef14c7ec1b51
JongMin22 <bbgs0605@naver.com>
2022-03-06 14:02:26 +0900

    UserVO, UserDAO 작업

---
+++ ICT_MARKET/src/main/java/kr/co/ict/UserVO.java
@@ -0,0 +1,55 @@
+package kr.co.ict;
+
+public class UserVO {
+	
+	private String user_id; 
+	private String user_pw; 
+	private String user_name; 
+	private String user_pnum; 
+	private String user_address;
+	public UserVO(String user_id, String user_pw, String user_name, String user_pnum, String user_address) {
+		super();
+		this.user_id = user_id;
+		this.user_pw = user_pw;
+		this.user_name = user_name;
+		this.user_pnum = user_pnum;
+		this.user_address = user_address;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_pw() {
+		return user_pw;
+	}
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_pnum() {
+		return user_pnum;
+	}
+	public void setUser_pnum(String user_pnum) {
+		this.user_pnum = user_pnum;
+	}
+	public String getUser_address() {
+		return user_address;
+	}
+		public void setUser_address(String user_address) {
+		this.user_address = user_address;
+	}
+	@Override
+	public String toString() {
+		return "UserVO [user_id=" + user_id + ", user_pw=" + user_pw + ", user_name=" + user_name + ", user_pnum="
+				+ user_pnum + ", user_address=" + user_address + "]";
+	} 
+	
+}
+
