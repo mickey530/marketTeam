32b9fe90822dbf7f53bb6c0c8479e1f47a666237
mickey530 <mickey530@naver.com>
2022-03-08 15:47:56 +0900

    최한결 : LoginServlet 로직 수정, 메인페이지 수정
    LoginServlet.java, main.jsp

--- ICT_MARKET/src/main/java/kr/co/ict/servlet/LoginServlet.java
+++ ICT_MARKET/src/main/java/kr/co/ict/servlet/LoginServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.co.ict.UserDAO;
+import kr.co.ict.UserVO;
 
 /**
  * Servlet implementation class LoginServlet
@@ -36,15 +40,33 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			// form에서 받아온 데이터 
 		    String fId = request.getParameter("fid");
 			String fPw = request.getParameter("fpw");
+			
+			UserDAO dao = UserDAO.getInstance();
+			UserVO user = dao.getUserData(fId);
 	
-			 request.setAttribute("id",fId);
-			    request.setAttribute("pw",fPw);
-			    
-			    
-			    RequestDispatcher dp = request.getRequestDispatcher("users/login_check.jsp");
-			    dp.forward(request, response);
+			// 유저 아이디와 비밀번호가 DB에 있는 데이터와 일치한다면
+			if(fId.equals(user.getUser_id()) && fPw.equals(user.getUser_pw())) {
+				// 세션 발급
+				HttpSession session = request.getSession(); 
+				session.setAttribute("sId", fId);
+				System.out.println("로그인에 성공했습니다.");
+				response.sendRedirect("http://localhost:8181/ICT_MARKET/");
+			} else {
+				// 일치하지 않는다면
+				// arlert로 로그인 실패 메세지 띄운 뒤 다시 로그인 페이지로 리다이렉트 할 예정
+				System.out.println("로그인에 실패했습니다.");
+				response.sendRedirect("http://localhost:8181/ICT_MARKET/users/login_form.jsp");
+			}
+		
+			
+
+			
+			// 포워딩이 필요한가?
+			RequestDispatcher dp = request.getRequestDispatcher("users/login_check.jsp");
+			dp.forward(request, response);
 	}
 
 }
