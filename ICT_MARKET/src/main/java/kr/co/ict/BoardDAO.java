c7ed6f148a68e8ae266c10829325c6d926d1bb74
mickey530 <mickey530@naver.com>
2022-03-10 16:56:55 +0900

    최한결 : 보드 테이블 변경사항 추가
    BoardVO, BoardDAO

--- ICT_MARKET/src/main/java/kr/co/ict/BoardDAO.java
+++ ICT_MARKET/src/main/java/kr/co/ict/BoardDAO.java
@@ -52,21 +52,21 @@ public class BoardDAO {
 			rs = pstmt.executeQuery();
 
 			while(rs.next()) {
+				int board_num = rs.getInt("board_num");
 				String user_id = rs.getString("user_id");
 				boolean board_info = rs.getBoolean("board_info");
-				int board_num = rs.getInt("board_num");
-				String board_content = rs.getString("board_content");
 				String board_category = rs.getString("board_category");
+				String board_title = rs.getString("board_title");
+				String board_content = rs.getString("board_content");
 				int board_amount = rs.getInt("board_amount");
 				boolean board_sold =  rs.getBoolean("board_sold");
-				String board_title = rs.getString("board_title");
+				int board_hit = rs.getInt("board_hit");
 				int board_reported = rs.getInt("board_reported");
 				Date board_writetime = rs.getDate("board_writetime");
 				Date board_updatetime = rs.getDate("board_updatetime");
 
 				
-				BoardVO boardData = new BoardVO(user_id, board_info, board_num, board_content,
-						board_category, board_amount, board_sold, board_title, board_reported, board_writetime, board_updatetime);
+				BoardVO boardData = new BoardVO(board_num, user_id, board_info, board_category, board_title, board_content, board_amount, board_sold, board_hit, board_reported, board_writetime, board_updatetime);
 				boardList.add(boardData);
 				
 				
@@ -87,24 +87,25 @@ public class BoardDAO {
 	
 	
 	
-	public void insertBoard(String user_id, int board_info, String board_content,
-			String board_category, String board_amount, int board_sold, String board_title ,String board_reported) 
+	public void insertBoard(String user_id, boolean board_info, String board_content,
+			String board_category, int board_amount, boolean board_sold, String board_title , int board_reported) 
 		{
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		try {
 			con = ds.getConnection();
 			
-			String sql = "INSERT INTO board(user_id, board_info, board_content, board_category, board_amount, board_sold, board_title, board_reported)VALUES(?,?,?)";
+			String sql = "INSERT INTO board(user_id, board_info, board_content, board_category, board_amount, board_sold, board_title,)"
+					+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, user_id);
-			pstmt.setInt(2, board_info);
+			pstmt.setBoolean(2, board_info);
 			pstmt.setString(3, board_content);
 			pstmt.setString(4, board_category);
-			pstmt.setString(5, board_amount);
-			pstmt.setInt(6, board_sold);
+			pstmt.setInt(5, board_amount);
+			pstmt.setBoolean(6, board_sold);
 			pstmt.setString(7, board_title);
-			pstmt.setString(8, board_reported);
+			pstmt.setInt(8, board_reported);
 		
 			pstmt.executeUpdate();
 		} catch(Exception e) {
