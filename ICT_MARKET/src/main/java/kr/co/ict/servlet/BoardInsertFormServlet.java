76607f4877ef5d6edc07b21a805d6bcb0526170d
mickey530 <mickey530@naver.com>
2022-03-15 14:35:08 +0900

    최한결 : Board CRUD 연결

--- ICT_MARKET/src/main/java/kr/co/ict/servlet/BoardInsertFormServlet.java
+++ ICT_MARKET/src/main/java/kr/co/ict/servlet/BoardInsertFormServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class BoardInsertPostingSevlet
  */
-@WebServlet("/boardInsertPosting")
+@WebServlet("/boardInsertForm")
 public class BoardInsertFormServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -28,7 +28,7 @@ public class BoardInsertFormServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher dp = request.getRequestDispatcher("/ICT_MARKET/board/Board_Insertform.jsp");
+		RequestDispatcher dp = request.getRequestDispatcher("/board/Board_Insertform.jsp");
 		dp.forward(request, response);
 	}
 
