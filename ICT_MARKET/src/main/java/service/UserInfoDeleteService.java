7632d84cad5db9fb7ba6e61a19f81541b6f91759
mickey530 <mickey530@naver.com>
2022-03-18 15:54:09 +0900

    최한결 UserInfoDeleteServie 수정

--- ICT_MARKET/src/main/java/service/UserInfoDeleteService.java
+++ ICT_MARKET/src/main/java/service/UserInfoDeleteService.java
@@ -14,15 +14,12 @@ public class UserInfoDeleteService implements boardInterface_Service{
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
 		String user_id = request.getParameter("user_id");
-		String struNum = request.getParameter("user_num");
-		int uNum = Integer.parseInt(struNum);
 		HttpSession session = request.getSession();
 		String sId = (String)session.getAttribute("session_id");
-		// 1. dao积己
+		
 		if(sId == user_id) {
 		UserDAO dao = UserDAO.getInstance();
-		// 2. 昏力肺流 角青
-		dao.deleteUser(uNum);
+		dao.deleteUser(user_id);
 		}
 	}
 
