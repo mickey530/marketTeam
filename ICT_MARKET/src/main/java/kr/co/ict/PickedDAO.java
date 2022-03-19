af548cb9db99d45f373a74ee437362a943898689
mickey530 <mickey530@naver.com>
2022-03-20 02:17:29 +0900

    최한결 : getAllPickedList (sell, buy, all) , pick 메서드 추가

--- ICT_MARKET/src/main/java/kr/co/ict/PickedDAO.java
+++ ICT_MARKET/src/main/java/kr/co/ict/PickedDAO.java
@@ -42,7 +42,7 @@ public class PickedDAO {
 		
 		try {
 			con = ds.getConnection();
-			String getInfo = "SELECT * FROM pickedlist WHERE picked_id = ?";
+			String getInfo = "SELECT * FROM pickedlist WHERE picked_id = ? ORDER BY picked_num DESC";
 			pstmt = con.prepareStatement(getInfo);
 			pstmt.setString(1, session_id);
 			rs = pstmt.executeQuery();
@@ -72,4 +72,72 @@ public class PickedDAO {
 		}
 		return PickedList;
 	}
+	
+	// board_info - true : sell / false : buy
+	public List<PickedVO> getAllPickedList(String session_id, Boolean board_info){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<PickedVO> PickedList = new ArrayList<>();
+		
+		try {
+			con = ds.getConnection();
+			String getInfo = "SELECT * FROM pickedlist WHERE picked_id = ? AND picked_board_info = ? ORDER BY picked_num DESC";
+			pstmt = con.prepareStatement(getInfo);
+			pstmt.setString(1, session_id);
+			rs = pstmt.executeQuery();
+
+			while(rs.next()) {
+				String picked_id = rs.getString("picked_id");
+				boolean picked_board_info = rs.getBoolean("picked_board_info");
+				int picked_board_num = rs.getInt("picked_board_num");
+				int picked_num = rs.getInt("picked_num");
+				
+				PickedVO pickedData = new PickedVO(picked_id, picked_board_info, picked_board_num, picked_num);
+				PickedList.add(pickedData);
+				}
+			
+			
+		} catch(Exception e){
+			e.printStackTrace();		
+		} finally{
+			
+			try {
+				con.close();
+				pstmt.close();
+				rs.close();
+			} catch(SQLException se) {
+				se.printStackTrace();		
+			}
+		}
+		return PickedList;
+	}
+	
+	public void Pick(String session_id, Boolean picked_board_info, int picked_board_num){
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = ds.getConnection();
+			String pick = "INSERT INTO picked(picked_id, picked_board_info, picked_board_num VALUES(?, ?, ?)";
+			pstmt = con.prepareStatement(pick);
+			pstmt.setString(1, session_id);
+			pstmt.setBoolean(2, picked_board_info);
+			pstmt.setInt(3, picked_board_num);
+			pstmt.executeUpdate();
+			
+		} catch(Exception e){
+			e.printStackTrace();		
+		} finally{
+			
+			try {
+				con.close();
+				pstmt.close();
+			} catch(SQLException se) {
+				se.printStackTrace();		
+			}
+		}
+	}
+	
+	
 }
