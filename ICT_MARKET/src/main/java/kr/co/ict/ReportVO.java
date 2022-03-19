2393b8d471e9a6d74d2f674acdaab5f7a05ddc4b
mickey530 <mickey530@naver.com>
2022-03-20 01:52:28 +0900

    최한결 : reportlist 테이블에 reporting_id, reported_num 컬럼 추가 및 VO 수정

--- ICT_MARKET/src/main/java/kr/co/ict/ReportVO.java
+++ ICT_MARKET/src/main/java/kr/co/ict/ReportVO.java
@@ -1,16 +1,37 @@
 package kr.co.ict;
 
 public class ReportVO {
-
+	
+	private int reported_num;
+	private String reporting_id;
 	private String reported_id;
 	private int reported_board_num;
 	private String reported_reason;
 	
-	public ReportVO(String reported_id, int reported_board_num, String reported_reason) {
+	public ReportVO(int reported_num, String reporting_id, String reported_id, int reported_board_num,
+			String reported_reason) {
 		super();
+		this.reported_num = reported_num;
+		this.reporting_id = reporting_id;
 		this.reported_id = reported_id;
 		this.reported_board_num = reported_board_num;
 		this.reported_reason = reported_reason;
+	}
+
+	public int getReported_num() {
+		return reported_num;
+	}
+
+	public void setReported_num(int reported_num) {
+		this.reported_num = reported_num;
+	}
+
+	public String getReporting_id() {
+		return reporting_id;
+	}
+
+	public void setReporting_id(String reporting_id) {
+		this.reporting_id = reporting_id;
 	}
 
 	public String getReported_id() {
@@ -39,7 +60,9 @@ public class ReportVO {
 
 	@Override
 	public String toString() {
-		return "ReportVO [reported_id=" + reported_id + ", reported_board_num=" + reported_board_num
-				+ ", reported_reason=" + reported_reason + "]";
+		return "ReportVO [reported_num=" + reported_num + ", reporting_id=" + reporting_id + ", reported_id="
+				+ reported_id + ", reported_board_num=" + reported_board_num + ", reported_reason=" + reported_reason
+				+ "]";
 	}
+	
 }
