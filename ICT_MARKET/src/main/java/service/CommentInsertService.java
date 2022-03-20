4bc147aae6085819e4e9e6c126deabcc39580e78
mickey530 <mickey530@naver.com>
2022-03-21 00:32:00 +0900

    최한결 : CommentService (Insert, Delete, Update)

---
+++ ICT_MARKET/src/main/java/service/CommentInsertService.java
@@ -0,0 +1,23 @@
+package service;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import kr.co.ict.BoardCommentDAO;
+
+public class CommentInsertService implements boardInterface_Service{
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) {
+		HttpSession session = request.getSession();
+		String comment_id = (String)session.getAttribute("session_id");
+		int board_num = Integer.parseInt(request.getParameter("board_num"));
+		String comment_content = request.getParameter("comment_content");
+		
+		
+		BoardCommentDAO dao = BoardCommentDAO.getInstance();
+		dao.insertComment(comment_id, board_num, comment_content);
+	}
+
+}
