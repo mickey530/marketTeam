c6f3ca8c8bd213a7fbb204f5fdaac1fd47bf9dda
mickey530 <mickey530@naver.com>
2022-03-27 04:38:15 +0900

    최한결 : 해당 게시물 전체 찜 데이터 삭제 로직

--- ICT_MARKET/src/main/java/service/BoardDeleteService.java
+++ ICT_MARKET/src/main/java/service/BoardDeleteService.java
@@ -15,20 +15,24 @@ public class BoardDeleteService implements boardInterface_Service{
 	public void execute(HttpServletRequest request, HttpServletResponse response) {
 		String boardnum = request.getParameter("board_num");
 		int board_num = Integer.parseInt(boardnum);
-		
 		String user_id = request.getParameter("user_id");
+		System.out.println("유저 : " + user_id);
+
 		
 		HttpSession session = request.getSession();
 		String sId = (String)session.getAttribute("session_id");		
 		
-		int page_num = Integer.parseInt(request.getParameter("page_num"));
-		request.setAttribute("page_num", page_num);
+		System.out.println("보드넘 : " + board_num);
+		
+		
+		
+		request.setAttribute("board_info", "ALL");
 		request.setAttribute("board_num", board_num);
 		
 		if(sId.equals(user_id)) {
 		
 		PickedDAO pDao = PickedDAO.getInstance();
-		pDao.pickDelete(board_num, user_id);
+		pDao.pickDelete(board_num);
 			
 		BoardCommentDAO cDao = BoardCommentDAO.getInstance();
 		cDao.deleteBoardComment(board_num);
